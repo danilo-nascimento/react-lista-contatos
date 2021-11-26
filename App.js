02def818c6f9a54f1c7a83a5f20489faeefd2c8d
Danilo Nascimento <danilo.nascimento.webdesign@gmail.com>
2021-11-25 22:53:46 -0300

    refactor: remoção do redux-thunk

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import container from './navegacao/ContatosNavigator';
 import { createStore, combineReducers, applyMiddleware } from 'redux' // Ficar atento que tudo isto é do Redux
 import { Provider } from 'react-redux';
-import reduxThunk from 'redux-thunk';
 import contatosReducer from './store/contatos-reducer';
 
 /**

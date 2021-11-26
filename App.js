e13105a9f9251db5cb52eadc9903a7073226d7aa
Danilo Nascimento <danilo.nascimento.webdesign@gmail.com>
2021-11-26 17:21:00 -0300

    remoção do mósulo não utilizado

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import container from './navegacao/ContatosNavigator';
-import { createStore, combineReducers, applyMiddleware } from 'redux' 
+import { createStore, combineReducers } from 'redux' 
 import { Provider } from 'react-redux';
 import contatosReducer from './store/contatos-reducer';
 
